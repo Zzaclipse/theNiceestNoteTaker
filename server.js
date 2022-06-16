7db205b4599767a3818db07f0aebe17707aad773
astokes <za.stokes@gmail.com>
2022-06-16 17:05:50 -0400

    server fully functional

--- server.js
+++ server.js
@@ -1,15 +1,101 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const { v4: uuidv4 } = require("uuid");
+const { clog } = require("./middleware/clog");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(clog);
+
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+const readAndDelete = (id, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      let i = 0;
+      let foundEntry;
+      parsedData.forEach((e) => {
+        if (e.id === id) {
+          foundEntry = i;
+        }
+        i++;
+      });
+      parsedData.splice(foundEntry, 1);
+
+      writeToFile(file, parsedData);
+    }
+  });
+};
 
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
+
+app.get("/api/notes", (req, res) =>
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+);
+
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+    readAndAppend(newNote, "./db/db.json");
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json("Error in posting feedback");
+  }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+
+  readAndDelete(id, "./db/db.json");
+  const response = {
+    status: "success",
+    body: id,
+  };
+
+  res.json(response);
+});
 
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
